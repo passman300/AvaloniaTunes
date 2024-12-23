d346d2fbbc2b8203438ba30af0c895b52fc0a004
Colin Wang <34583138+passman300@users.noreply.github.com>
2024-12-23 18:36:30 -0500

    Finished Dialog Content

--- Avalonia.MusicStore/Views/MainWindow.axaml.cs
+++ Avalonia.MusicStore/Views/MainWindow.axaml.cs
@@ -1,11 +1,28 @@
-using Avalonia.Controls;
+using System.Threading.Tasks;
+using ReactiveUI;
+using Avalonia.MusicStore.ViewModels;
+using Avalonia.ReactiveUI;
 
 namespace Avalonia.MusicStore.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+        
+        this.WhenActivated(action => 
+            action(ViewModel!.ShowDialog.RegisterHandler(DoShowDialogAsync)));
+    }
+    
+    // This code is only valid in newer ReactiveUI which is shipped since avalonia 11.2.0 
+    private async Task DoShowDialogAsync(IInteractionContext<MusicStoreViewModel,
+        AlbumViewModel?> interaction)
+    {
+        var dialog = new MusicStoreWindow();
+        dialog.DataContext = interaction.Input;
+
+        var result = await dialog.ShowDialog<AlbumViewModel?>(this);
+        interaction.SetOutput(result);
     }
 }
