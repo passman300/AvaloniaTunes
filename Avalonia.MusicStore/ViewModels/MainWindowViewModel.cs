57a0cb23d94648a98765052fe9af1d469a53bab6
Colin Wang <34583138+passman300@users.noreply.github.com>
2024-12-23 16:32:44 -0500

    First Commit. Created the album and store main page

---
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace Avalonia.MusicStore.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand BuyMusicCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            BuyMusicCommand = ReactiveCommand.Create(() =>
+            {
+                // Code here will be executed when the button is clicked.
+            });
+        }
+    }
+}
