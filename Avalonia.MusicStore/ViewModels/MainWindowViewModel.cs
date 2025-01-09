a6baadc087c4941b4e75309634cc13796d849de3
Colin Wang <34583138+passman300@users.noreply.github.com>
2025-01-08 20:08:28 -0500

    Squashed commit of the following:
    commit ac371a04d4488b1f4fe4e804e2f4c5d5a33b0b36
    Author: Colin Wang <34583138+passman300@users.noreply.github.com>
    Date:   Thu Dec 26 18:42:07 2024 -0500
    
        Failed Implmentation of Sidebar
    
        There is issue of mixing reactiveUI with MVV

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Reactive.Linq;
 using System.Text;
 using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using ReactiveUI;
 
 namespace Avalonia.MusicStore.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ViewModelBase
     {
-        public MainWindowViewModel()
+        /*public MainWindowViewModel()
         {
             ShowDialog = new Interaction<MusicStoreViewModel, SongViewModel?>();
 
@@ -19,9 +21,20 @@ namespace Avalonia.MusicStore.ViewModels
 
                 var result = await ShowDialog.Handle(store);
             });
+        }*/
+        
+        [ObservableProperty]
+        private bool _isPaneOpen = true;
+        
+        // TODO
+        [RelayCommand]
+        public void TriggerPaneComand()
+        {
+            IsPaneOpen = !IsPaneOpen;
+            //https://www.youtube.com/watch?v=UDbKVheMBY8&list=PLJYo8bcmfTDF6ROxC8QMVw9Zr_3Lx4Lgd&index=3
         }
         
-        public ICommand BuyMusicCommand { get; }
+        /*public ICommand BuyMusicCommand { get; }
 
         public Interaction<MusicStoreViewModel, SongViewModel?> ShowDialog { get; }
     }
